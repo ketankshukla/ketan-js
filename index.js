5d3ff4ebdcf595fa29e121c7a448e275f5c9b166
Ketan <ketankshukla@gmail.com>
2021-01-08 02:42:52 -0800

    modified again

--- index.js
+++ index.js
@@ -14,4 +14,23 @@ y
 const g = add(45,56)
 g
 
+function hello(a,b) {
+
+}
+
+
+const addition = function(a,b) {
+    return a+b
+};
+
+const z = addition(2,3)
+z
+
+const tester = (tester2) => {
+    return tester2 * 2
+};
+
+const yy = tester(78)
+yy
+
 
