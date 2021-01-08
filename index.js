cc0b52fdb7e1b602f866f4a2c6c9762dd181f284
Ketan <ketankshukla@gmail.com>
2021-01-08 03:40:46 -0800

    FizzBuzz functional version 1

--- index.js
+++ index.js
@@ -45,3 +45,14 @@ const yy = tester(78)
 yy
 
 
+//Functional FizzBuzz version 1
+
+console.log(
+  Array.apply(null, {length: 100}).map(function(val, index) {
+    index++;
+    if (index % 15 == 0){return "FizzBuzz";}
+    if (index % 3 == 0){return "Fizz";}
+    if (index % 5 == 0){return "Buzz";}
+    return index;
+  }).join('\n')
+);
